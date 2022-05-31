555389b995c856782067ab6767f9617dc7fad63a
Inma Sánchez Casabán <inma.sanchezcasaban@maibornwolff.de>
2022-05-31 15:47:20 +0200

    add exercises 1 & 2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,15 +3,37 @@
 
 import React from 'react'
 
-function countReducer(count, step) {
-  return count + step
+// const countReducer = (state, action) => ({
+//   ...state,
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
+  }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
-  const increment = () => changeCount(step)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
+  // const increment = () =>
+  //   setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
+
+// function Counter({initialCount = 0, step = 1}) {
+//   const [count, changeCount] = React.useReducer(countReducer, initialCount)
+//   const increment = () => changeCount(step)
+// }
 
 function App() {
   return <Counter />
